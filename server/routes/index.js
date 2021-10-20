b0802cd6d82b5832009d80ef5f357733f4526add
Prue Singh <pruesinghnagra@gmail.com>
2021-10-20 23:43:58 +1300

    added route to /id

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,3 +15,16 @@ router.get('/', (req, res) => {
     res.render('home', data)
   })
 })
+
+router.get('/puppies/:id', (req, res) => {
+  getData((err, data) => {
+    if (err) {
+      return res.status(500).send(err.message)
+    }
+
+    const id = Number(req.params.id)
+    const puppyDetails = data.puppies.find(puppy => puppy.id === id)
+
+    res.render('details', puppyDetails)
+  })
+})
