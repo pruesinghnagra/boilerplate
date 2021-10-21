33325ae10295913dfc3a46d1f41d0382770f99e9
Prue Singh <pruesinghnagra@gmail.com>
2021-10-21 21:40:14 +1300

    added edit/id routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { getData } = require('../utils')
+const { getData, writeData } = require('../utils')
 
 module.exports = router
 
@@ -26,5 +26,35 @@ router.get('/puppies/:id', (req, res) => {
     const puppyDetails = data.puppies.find(puppy => puppy.id === id)
 
     res.render('details', puppyDetails)
+  })
+})
+
+router.get('/puppies/:id/edit', (req, res) => {
+  getData((err, data) => {
+    if (err) {
+      return res.status(500).send(err.message)
+    }
+
+    const id = Number(req.params.id)
+    const puppyDetails = data.puppies.find(puppy => puppy.id === id)
+
+    res.render('edit', puppyDetails)
+  })
+})
+
+router.post('/puppies/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  const { name, breed, owner, image } = req.body
+  const updatedPuppy = { id, name, breed, owner, image }
+  
+  writeData(updatedPuppy, (err) => {
+    if (err) {
+      err.code === 404
+      ? res.sendStatus(404)
+      : res.status(500).send(err.message)
+      return
+    }
+
+    res.redirect('/puppies/' + id)
   })
 })
