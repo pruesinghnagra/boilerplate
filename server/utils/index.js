ee67c0f65d2abadf9ae068d11ba7f1d38ad935b8
Prue Singh <pruesinghnagra@gmail.com>
2021-10-21 21:40:44 +1300

    added writeData function

--- server/utils/index.js
+++ server/utils/index.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  getData
+  getData,
+  writeData
 }
 
 const filepath = path.join(__dirname, '../', 'data', 'data.json')
@@ -21,5 +22,41 @@ function getData (cb) {
       console.error(err)
       cb(new Error('Oh no! Unable to parse the data!')) 
     }
+  })
+}
+
+function writeData (updatedData, cb) {
+  getData((err, data) => {
+    if (err) {
+      return res.status(500).send(err.message)
+    }
+    
+    const editThisPuppy = data.puppies.find(puppy => puppy.id === updatedData.id)
+    if (!editThisPuppy) {
+      console.error('Unable to find data')
+      cb(new Error('Oh no! ID was not found!'))
+      return
+    }
+
+    const { name, breed, owner, image} = updatedData
+    editThisPuppy.name = name
+    editThisPuppy.breed = breed
+    editThisPuppy.owner = owner
+    editThisPuppy.image = image
+    
+    const updatedPuppies = {
+      ...data
+    }
+
+    const stringifyData = JSON.stringify(updatedPuppies,  null, 2)
+
+    fs.writeFile(filepath, stringifyData, 'utf8', (err) => {
+      if (err) {
+        console.error('Unable to write to that file')
+        cb(new Error('Oh no! That file does not want to be written to!'))
+        return
+      }
+      cb()
+    })
   })
 }
